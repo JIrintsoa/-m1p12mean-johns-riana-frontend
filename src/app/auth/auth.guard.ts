2f69d1706998f6110ac6e2e53bfa87a0b2a809a9
Johns <johns.irintsoa@gmail.com>
2025-03-19 15:24:35 +0300

    Update the login features

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -12,15 +12,26 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot): boolean {
     const token = this.authService.getToken();
     const expectedRole = route.data['roles'] as string[];
+    const userRole = this.authService.getUserRole();
 
     if (!token) {
+      // Si aucun token n'est trouvé, vérifier si l'utilisateur est sur la page de connexion
+      if (route.routeConfig?.path !== 'login') {
         this.router.navigate(['/login']);
         return false;
+      }
+      return true; // Autoriser l'accès à la page de connexion
+    }
+    else if (route.routeConfig?.path === 'login'){
+      this.redirectToHome(userRole);
+      return false;
     }
 
-    const userRole = this.authService.getUserRole();
-    console.log(`Rôle de l'utilisateur : ${userRole}`);
-    console.log(`Rôle(s) attendu(s) : ${expectedRole}`);
+    // Si l'utilisateur est déjà connecté et tente d'accéder à la page de connexion, le rediriger
+    // if (route.routeConfig?.path === 'login') {
+    //   this.redirectToHome(userRole);
+    //   return false;
+    // }
 
     if (!expectedRole || expectedRole.length === 0 || expectedRole.includes(userRole)) {
         return true; // L'accès est autorisé si aucun rôle spécifique n'est requis ou si le rôle de l'utilisateur correspond à un rôle attendu
@@ -28,11 +39,17 @@ export class AuthGuard implements CanActivate {
         this.router.navigate(['/unauthorized']); // Rediriger vers une page d'accès non autorisé
         return false;
     }
-    // if (localStorage.getItem('token')) {
-    //   return true;
-    // } else {
-    //   this.router.navigate(['/login']);
-    //   return false;
-    // }
+  }
+
+  private redirectToHome(userRole: string | null): void {
+    if (userRole === 'manager') {
+      this.router.navigate(['/manager/appointment-list']);
+    } else if (userRole === 'mechanic') {
+      this.router.navigate(['/mechanic/repair-list']);
+    } else if (userRole === 'client') {
+      this.router.navigate(['/client/take-appointment']);
+    } else {
+      this.router.navigate(['/']); // Rediriger vers une page par défaut si le rôle n'est pas reconnu
+    }
   }
 }
