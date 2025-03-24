9152cc82970c30eed79afd96411b456cac940a7d
rianarahajary <89478572+rianarahajary@users.noreply.github.com>
2025-03-24 12:32:34 +0300

    crud-mecanicien

--- src/app/services/mechanic/mechanic.service.ts
+++ src/app/services/mechanic/mechanic.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,15 @@ export class MechanicService {
 
   constructor(private apiService: ApiService) { }
 
-  getMechanics(page: number = 1, limit: number = 10, filter: string = ''): Observable<unknown> {
+  getMechanics(page: number = 1, limit: number = 10, search: string = '', status: string = '', token: string): Observable<unknown> {
     const url = `/mechanics`;
     const params = {
       page: page.toString(),
       limit: limit.toString(),
-      filter: filter 
+      search,
+      status
     };
-    return this.apiService.get<unknown>(url,params);
+    return this.apiService.get<unknown>(url, params, token);
   }
 
   getActiveMechanics(page: number = 1, limit: number = 10, filter: string = ''): Observable<unknown> {
@@ -24,13 +26,18 @@ export class MechanicService {
     const params = {
       page: page.toString(),
       limit: limit.toString(),
-      filter: filter 
+      filter: filter
     };
-    return this.apiService.get<unknown>(url,params);
+    return this.apiService.get<unknown>(url, params);
   }
 
-  addMechanic(mechanicData: unknown): Observable<unknown> {
+  addMechanic(mechanicData: unknown, token: string): Observable<unknown> {
     const url = '/mechanics';
-    return this.apiService.post(url, mechanicData);
+    return this.apiService.post(url, mechanicData, token);
+  }
+
+  updateMechanic(mechanic: User, token: string): Observable<unknown> {
+    const url = `/mechanics/${mechanic._id}`;
+    return this.apiService.put(url, mechanic, token);
   }
 }
