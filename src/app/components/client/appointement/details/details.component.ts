d894aabe952695a2950bf83ac934125a223eb9ee
rianarahajary <89478572+rianarahajary@users.noreply.github.com>
2025-03-25 14:54:18 +0300

    comment-client

--- src/app/components/client/appointement/details/details.component.ts
+++ src/app/components/client/appointement/details/details.component.ts
@@ -1,34 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbRatingModule  } from '@ng-bootstrap/ng-bootstrap';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AppointmentService } from 'src/app/services/appointment/appointment.service';
 import { AppointmentDetailResponse } from 'src/app/models/appointment.model';
 import { formatDate } from 'src/app/utils/utils.function';
+import { CommentService } from 'src/app/services/comment/comment.service';
+import { CommentModel } from 'src/app/models/comment.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-details',
-  imports: [NgbRatingModule, ReactiveFormsModule],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+	selector: 'app-details',
+	imports: [NgbRatingModule, ReactiveFormsModule, CommonModule, FormsModule],
+	templateUrl: './details.component.html',
+	styleUrl: './details.component.scss'
 })
 
 
 export class DetailsComponent implements OnInit {
+	token: string;
 	appointmentId: string | null = null;
 	appointmentDetailResponse: AppointmentDetailResponse = null;
 	rating = 2
 	maxRate = 5
 	ctrl = new FormControl<number | null>(null, Validators.required);
+	comments: CommentModel[] = []
+	newComment: string
+	successMessage: string = '';
+	errorMessage: string = '';
 
 	formatDate = formatDate;
 
 	constructor(
 		private route: ActivatedRoute,
 		private http: HttpClient,
-		private appointmentService: AppointmentService
-	){}
+		private appointmentService: AppointmentService,
+		private commentService: CommentService,
+		private authService: AuthService,
+	) {
+		this.token = authService.getToken();
+	}
 
 	ngOnInit(): void {
 
@@ -38,9 +51,10 @@ export class DetailsComponent implements OnInit {
 				this.fetchAppointmentDetails(this.appointmentId);
 			}
 		});
+		this.fetchComments();
 	}
 
-	fetchAppointmentDetails(appointmentId: string){
+	fetchAppointmentDetails(appointmentId: string) {
 		this.appointmentService.getAppointmentById(appointmentId).subscribe({
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			next: (response: AppointmentDetailResponse) => {
@@ -52,8 +66,8 @@ export class DetailsComponent implements OnInit {
 				// this.appointmentDetail = result.items || [];
 				// console.log(this.appointmentDetail)
 				// console.log('Processed vehicles:', this.vehicles);
-			  },
-			  error: (error) => {
+			},
+			error: (error) => {
 
 				console.error('Error fetching vehicles:', error);
 			}
@@ -66,5 +80,54 @@ export class DetailsComponent implements OnInit {
 		} else {
 			this.ctrl.disable();
 		}
+	}
+	fetchComments() {
+		this.commentService.getComments(this.appointmentId, this.token).subscribe({
+			next: (response: any) => {
+				const result = response;
+				this.comments = result.items || [];
+			},
+			error: (error) => {
+				console.error('Error fetching vehicles:', error);
+			}
+		});
+	}
+
+	addComment(): void {
+		this.commentService.addComment(this.appointmentId, this.newComment, this.token).subscribe({
+			next: (response) => {
+				console.log('Vehicle added successfully:', response);
+				this.showMessage('Véhicule ajouté avec succès!', 'success');
+				this.fetchComments(); // Refresh the vehicle list
+				this.resetForm(); // Optionally reset the form after submission
+			},
+			error: (error) => {
+				this.showMessage('Erreur lors de l\'ajout du véhicule.', 'error');
+				console.error('Error adding vehicle:', error);
+			}
+		});
+	}
+
+	resetForm(): void {
+		this.newComment = '';
+	}
+
+	showMessage(message: string, type: 'success' | 'error'): void {
+		if (type === 'success') {
+			this.successMessage = message;
+			this.errorMessage = '';
+		} else {
+			this.errorMessage = message;
+			this.successMessage = '';
+		}
+
+		// Hide the message after 3 seconds
+		setTimeout(() => {
+			if (type === 'success') {
+				this.successMessage = '';
+			} else {
+				this.errorMessage = '';
+			}
+		}, 10000);
 	}
 }
