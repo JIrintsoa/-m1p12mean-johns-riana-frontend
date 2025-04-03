5a2d29a9cc71e14137cf06753a168de2c262b75b
Johns <johns.irintsoa@gmail.com>
2025-04-03 20:23:47 +0300

    remove the space

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true, // Indique que c'est l'environnement de production
-  apiUrl: 'https://web-m1-backend.vercel.app/api ', // URL de l'API de production
+  apiUrl: 'https://web-m1-backend.vercel.app/api', // URL de l'API de production
 };
